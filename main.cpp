b7b1dc60656be44ab176fc789cd23b718c7cb8a7
vngkgk624 <vngkgk624@naver.com>
2016-12-03 13:38:10 +0900

    123

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTranslator>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTranslator qtTranslator;
+    qtTranslator.load(":/lang/lang_ko.qm");
+    a.installTranslator(&qtTranslator);
     MainWindow w;
     w.show();
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,9 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    temp_humi_server *tempserver = new temp_humi_server();
+    tempserver->show();
+}
