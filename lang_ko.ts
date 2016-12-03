45f077c08682bc04a975729f1c70156225db7549
vngkgk624 <vngkgk624@naver.com>
2016-12-03 13:43:34 +0900

    123

---
+++ lang_ko.ts
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>auto temp mail send</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>temp_humi_baseview</name>
+    <message>
+        <location filename="temp_humi_baseview.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="temp_humi_baseview.ui" line="28"/>
+        <source>chart</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>temp_humi_server</name>
+    <message>
+        <location filename="temp_humi_server.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="temp_humi_server.cpp" line="371"/>
+        <location filename="temp_humi_server.cpp" line="374"/>
+        <source>deposition</source>
+        <translation>성막</translation>
+    </message>
+    <message>
+        <location filename="temp_humi_server.cpp" line="377"/>
+        <location filename="temp_humi_server.cpp" line="380"/>
+        <source>etching1</source>
+        <translation>에칭1</translation>
+    </message>
+    <message>
+        <location filename="temp_humi_server.cpp" line="383"/>
+        <location filename="temp_humi_server.cpp" line="386"/>
+        <source>etching2</source>
+        <translation>에칭2</translation>
+    </message>
+    <message>
+        <location filename="temp_humi_server.cpp" line="389"/>
+        <location filename="temp_humi_server.cpp" line="392"/>
+        <source>probe1</source>
+        <translation>프로브1</translation>
+    </message>
+    <message>
+        <location filename="temp_humi_server.cpp" line="395"/>
+        <location filename="temp_humi_server.cpp" line="398"/>
+        <source>probe2</source>
+        <translation>프로브2</translation>
+    </message>
+    <message>
+        <location filename="temp_humi_server.cpp" line="473"/>
+        <source>temp</source>
+        <translation>온도</translation>
+    </message>
+    <message>
+        <location filename="temp_humi_server.cpp" line="485"/>
+        <location filename="temp_humi_server.cpp" line="505"/>
+        <location filename="temp_humi_server.cpp" line="531"/>
+        <source>Date</source>
+        <translation>날짜</translation>
+    </message>
+    <message>
+        <location filename="temp_humi_server.cpp" line="493"/>
+        <location filename="temp_humi_server.cpp" line="515"/>
+        <location filename="temp_humi_server.cpp" line="541"/>
+        <source>humi</source>
+        <translation>습도</translation>
+    </message>
+</context>
+</TS>
